37df094fb4c5b303b1cccaf2c33b8f7d3ec8fe9a
Mark Evan Delos Reyes <markevancoc@gmail.com>
2024-03-20 22:25:10 +0800

    added furina pop ups

--- script.js
+++ script.js
@@ -6,3 +6,42 @@ function myHamburger() {
         x.className = "navlinks";
     }
 }
+
+function popup() {
+    var x = document.getElementById("normalatkpp");
+    if (x.className === "popup") {
+        x.className += " enable";
+    } else {
+        x.className = "popup";
+    }
+
+    var y = document.getElementById("iconpopup");
+    if (y.className === "iconpopup") {
+        y.className += " enable";
+    } else {
+        y.className = "iconpopup";
+    }
+}
+
+function popup2() {
+    var x = document.getElementById("skillpp");
+    if (x.className === "popup2") {
+        x.className += " enable";
+    } else {
+        x.className = "popup2";
+    }
+}
+
+function popup3() {
+    var x = document.getElementById("burstpp");
+    if (x.className === "popup3") {
+        x.className += " enable";
+    } else {
+        x.className = "popup3";
+    }
+}
+
+
+
+
+
